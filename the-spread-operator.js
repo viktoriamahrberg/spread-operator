cdd7abd8264f20606564c2e59374fc0749cd4b98
Vikmah <viktoria.mahrberg@gmail.com>
2022-03-09 12:25:30 +0000

    Initial commit

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,27 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("Second array:", arr2);
+console.log("First array:", arr1);
 
 // Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+arr4.push(7);
 
+console.log(arr3);
+console.log(arr4);
 // Copying an object
 
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { ...obj1, d: 4};
+
+console.log(obj1);
+console.log(obj2);
 // Copying only part of an array/object
+
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+console.log(arr5);
